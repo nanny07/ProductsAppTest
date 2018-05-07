08c7f52dbf84fadbf9737bbe5d7f9e057b97b6bb
Gianni Bonicolini <nanny07@gmail.com>
2018-05-07 21:43:37 +0200

    Test

---
+++ ProductsAppTest/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ProductsAppTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsAppTest.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name="Prod. One", Category="Toys", Price = 1 },
+            new Product { Id = 2, Name="Prod. Two", Category="Hardware", Price = 3.75M },
+            new Product { Id = 3, Name="Prod. Tree", Category="Groceries", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
